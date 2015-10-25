8c83e89a9ce5dd884b05c6c3c70c8f6f6a821462
chuyano <spawner.astral@hotmail.com>
2015-10-25 14:22:27 -0600

    1

---
+++ newfile.php
@@ -0,0 +1,28 @@
+<html>
+
+<body>
+<form name="form1" method="post" action="Radio.php">
+numero1<br>
+<input type="text" name="text1">
+<br>
+numero2<br>
+<input type="text" name="text2">
+<br>
+<input type="radio" name="radio1" value="sumar">
+Sumar +
+<br>
+<input type="radio" name="radio1" value="restar">
+Restar -
+<br>
+<input type="radio" name="radio1" value="multiplicar">
+Multiplicar *
+<br>
+<input type="radio" name="radio1" value="dividir">
+Dividir /
+<br>
+<input type="submit" name="boton1" value="enviar">
+</form>
+</body>
+
+ 
+</html>
